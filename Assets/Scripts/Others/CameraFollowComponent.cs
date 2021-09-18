b9ea637600eaca2c993d8d2b8f2f4544121f5d9b
Jordy_Rodrigues <jordyrg616@gmail.com>
2021-09-18 01:43:08 -0300

    Adicionando Camera Follow ao jogador;

---
+++ Assets/Scripts/Others/CameraFollowComponent.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowComponent : MonoBehaviour
+{
+    [SerializeField] private Transform objectToFollow;
+    [SerializeField] private float followDistance;
+    [SerializeField] private float followSpeed;
+    private bool following;
+
+    void Start()
+    {
+
+    }
+
+    private IEnumerator Follow(Vector3 direction)
+    {
+        while(!Mathf.Approximately(direction.magnitude, 0))
+        {
+            transform.position += direction * followSpeed;
+            yield return new WaitForSecondsRealtime(.01f);
+            direction = objectToFollow.position - transform.position + new Vector3(0, 0, transform.position.z);
+        }
+
+        following = false;
+
+        StopCoroutine("Follow");
+    }
+
+    void Update()
+    {
+        Vector3 direction = objectToFollow.position - transform.position + new Vector3(0, 0, transform.position.z);
+        Debug.Log(direction.magnitude);
+        if(direction.magnitude >= followDistance && !following)
+        {
+            following = true;
+            StartCoroutine(Follow(direction));
+        }
+    }
+}
